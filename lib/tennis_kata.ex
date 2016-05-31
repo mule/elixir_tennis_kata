f6133189bb961e09399744f0bbaf9fe424a8a943
Jukka Puranen <jukka.puranen@gmail.com>
2016-05-31 17:03:06 +0300

    wip

--- lib/tennis_kata.ex
+++ lib/tennis_kata.ex
@@ -5,6 +5,7 @@ end
 defmodule TennisMatch do
     @point_scores  [:love, :fifteen, :thirty, :forty]
     @point_endgame_states  [:deuce, :p1_advantage, :p2_advantage]
+    @players [:p1, :p2]
     defstruct [match_type: :nil, point: {:love, :love}, match_status: :normal]
 
     def start_match(match_type) when match_type == :best_of_three_sets or match_type == :best_of_five_sets do
@@ -22,23 +23,42 @@ defmodule TennisMatch do
     def point(:p1_advantage), do: :p1_advantage
     def point(:p2_advantage), do: :p2_advantage
 
-    def next_point_score(ball_winner, curent_point)  when current_point in @point_endgame_states do
-         point_state = {ball_winner, curent_point}
-         case curent_point do
+    def next_point_score(ball_winner, current_point)
+    when ball_winner in @players and current_point in @point_endgame_states
+     do
+         params = {ball_winner, current_point}
+         case params do
              {:p1, :deuce} -> :p1_advantage
              {:p2, :deuce} -> :p2_advantage
              {:p1, :p1_advantage} -> :game_p1
              {:p2, :p2_advantage} -> :game_p2
              {:p1,:p2_advantage} -> :deuce
+             {:p2,:p1_advantage} -> :deuce
          end
     end
 
     def next_point_score(ball_winner, current_point)
     when is_tuple(current_point)
+    and ball_winner in @players
     and tuple_size(current_point) == 2
-    and elem(current_point,0) in @point_scores
-    and elem(current_point,1) in @point_scores do
+    and elem(current_point, 0) in @point_scores
+    and elem(current_point, 1) in @point_scores
+    and current_point != {:forty, :forty}
+    do
+        {p1_score, p2_score} = current_point
 
-        
+        cond  do
+            {:p1, {:love, _ }} == {ball_winner, current_point} -> {:fifteen, p2_score}
+            {:p2, {_, :love}} == {ball_winner, current_point} -> {p1_score, :fifteen}
+            {:p1, {:fifteen, _ }} =  {ball_winner, current_point} -> {:thirty, p2_score}
+            {:p2, {_, :fifteen}} = {ball_winner, current_point} -> {p1_score, :thirty}
+            {:p1, {:thirty, _ }} = {ball_winner, current_point} -> {:forty, p2_score}
+            {:p1, {:thirty, :forty}} = {ball_winner, current_point} -> :deuce
+            {:p2, { _ , :thirty}} = {ball_winner, current_point} -> {p1_score, :forty}
+            {:p2, {:forty, :thirty}} = {ball_winner, current_point} -> :deuce
+            {:p1, {:forty, _ }} = {ball_winner, current_point} -> :game_p1
+            {:p2, {_, :forty}} = {ball_winner, current_point} -> :game_p2
+        end
     end
+
 end
--- test/tennis_kata_test.exs
+++ test/tennis_kata_test.exs
@@ -19,4 +19,44 @@ defmodule TennisKataTest do
     test "should get point score" do
         TennisMatch.point(:love, :fifteen)
     end
+
+    test "Should get next point score" do
+
+
+
+        actual = TennisMatch.next_point_score(:p1, {:love, :love })
+        assert actual == {:fifteen, :love}
+        actual = TennisMatch.next_point_score(:p2, {:love, :love })
+        assert actual == {:love, :fifteen}
+        IO.IO.inspect actual
+        actual = TennisMatch.next_point_score(:p1, {:thirty, :forty })
+        assert actual == :deuce
+        actual = TennisMatch.next_point_score(:p2, {:forty, :thirty })
+        assert actual == :deuce
+
+
+
+        actual = TennisMatch.next_point_score(:p1, :deuce)
+        assert actual == :p1_advantage
+        actual = TennisMatch.next_point_score(:p2, :deuce)
+        assert actual == :p2_advantage
+        actual = TennisMatch.next_point_score(:p1, :p1_advantage)
+        assert actual == :game_p1
+        actual = TennisMatch.next_point_score(:p2, :p2_advantage)
+        assert actual == :game_p2
+        actual = TennisMatch.next_point_score(:p1, :p2_advantage)
+        assert actual == :deuce
+        actual = TennisMatch.next_point_score(:p2, :p1_advantage)
+        assert actual == :deuce
+    end
+
+    test "Should not be able to get next point score" do
+        catch_error TennisMatch.next_point_score(:p1, {:forty, :forty})
+        catch_error TennisMatch.next_point_score(:p4, {:forty, :forty})
+        catch_error TennisMatch.next_point_score(:p1, {:forty, :random})
+        catch_error TennisMatch.next_point_score(:p2, {:ramdp, :forty})
+        catch_error TennisMatch.next_point_score(:p1, {:dsgjdgklsj})
+        catch_error TennisMatch.next_point_score(:p1, {:forty, :thiry, :fifteen})
+        catch_error TennisMatch.next_point_score(:p1, {:fifteen})
+    end
 end
